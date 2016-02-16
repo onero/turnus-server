6c6d75ee93ba433c22ed3cc031911a925c88a2b6
Adamino <adamlars90@gmail.com>
2016-02-16 16:22:31 +0100

    server

--- server/server.js
+++ server/server.js
@@ -3,18 +3,26 @@ var boot = require('loopback-boot');
 
 var app = module.exports = loopback();
 
-app.start = function() {
-  // start the web server
-  return app.listen(function() {
-    app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
-    if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-    }
+app.start = function () {
+   // start the web server
+   var port = process.env.PORT || 8000;
+   return app.listen(port, function () {
+      app.emit('started      console.log('Webver listening at: %s', app.get('url
   });
 };
+
+// app.start = function() {
+//   // start the web server
+//   return app.listen(function() {
+//     app.emit('started');
+//     var baseUrl = app.get('url').replace(/\/$/, '');
+//     console.log('Web server listening at: %s', baseUrl);
+//     if (app.get('loopback-component-explorer')) {
+//       var explorerPath = app.get('loopback-component-explorer').mountPath;
+//       console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+//     }
+//   });
+// };
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
